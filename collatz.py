5a373f15d22c6f0a903e211fd9210ad91cc13a88
Shane Healy/SKBT <shane.healy1@skbiotek.com>
2019-09-22 12:54:21 +0100

    First draft of collatz.

---
+++ collatz.py
@@ -0,0 +1,14 @@
+#Shane Healy 22-SEP-2019
+
+n=20
+
+# Keep looping until reach 1
+# Assumes the Collatz conjecture is true
+while n != 1:
+    print(n)
+    if n%2 == 0:
+        n = n/2
+    else:
+        n = (3*n)+1
+
+print(n)
